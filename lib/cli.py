403eb45624683aaee8c9293adb73171f1849130a
Ashley Rivera <ashleyariv@gmail.com>
2023-09-27 15:18:05 -0400

    practically finished project

--- lib/cli.py
+++ lib/cli.py
@@ -1,107 +1,116 @@
-# lib/cli.py
+import ipdb
+from models.classes import Player, Result
 
-# from helpers import (
-#     exit_program,
-#     helper_1
-# )
+def main():
+    player = None
+    points = 0
+    
+    while True:
 
+        username = input('Enter adventerous username: ')
+        players = [player for player in Player.all if player.username == username]
+        if len(players) > 0:
+            player = players[0]
+            print(f'Welcome back, {player.username}! Get Ready to play again!')
+        else:
+            player = Player.create(username)
+            print(f'Welcome, {player.username}! Get ready to play, the rules are as follows:\nYou must choose a path at each prompt\nChoose carefully of it could cost you\nRemember every decision counts, so choose wisely, and have fun!!')
+            
+        answer = input('Do you wish to continue on this adventure? (y/n): ').lower()
 
-# def main():
-#     while True:
-#         menu()
-#         choice = input("> ")
-#         if choice == "0":
-#             exit_program()
-#         elif choice == "1":
-#             helper_1()
-#         else:
-#             print("Invalid choice")
-
-
-# def menu():
-#     print("Please select an option:")
-#     print("0. Exit the program")
-#     print("1. Some useful function")
-
-
-# if __name__ == "__main__":
-#     main()
-
-username = input('Enter adventerous username: ')
-print(f'Welcome, {username}! Get ready to play, the rules are as follows:\nYou must choose a path at each prompt\nChoose carefully of it could cost you\nRemember every decision counts, so choose wisely, and have fun!!')
-
-answer = input('Do you wish to continue on this adventure? (y/n): ').lower()
-
-while True:
-    if answer == 'n':
-        break
-    if answer == 'y':
-        answer = input("You've just woken up in a strange room. You hear a faint sound coming from upstairs and it sounds like something in distress. You also see what you think are flashlights roaming around outside. Do you follow the sound or the lights? Please Choose (sound/lights): ").lower()
-        if answer == 'sound':
-            answer = input('You head upstairs toward the sound and find a locked armoire. It sounds like there is a rabid dog inside, do you open it or go back downstairs? Please Choose (open/leave): ').lower()
-            if answer == 'leave':
-                print('You turn around to go back but fall down the stairs and break your neck.\nTry again next time! Thanks for playing!')
-                break
-            elif answer == 'open':
-                answer = input('A dog greets you with kisses, happy to be saved. Inside the armoire you find a crossbow but no arrows, do you take it or leave it behind? Please Choose (take it/leave it): ').lower()
-                if answer == 'take it':
-                    answer = input('You head downstairs and lucky for you the dog notices a big hole in the ground, so you step around it. You start calling for help moving towards the lights in the woods and you notice the lights begin to charge at you. It is a very large man looking to harm you but once he is close enough the dog jumps to your rescue and attcks. He successfully protects you but dies in the tussle. Do you go back into the house and hide or do you take the flashlight from the corpse and continue walking through the woods? Please Choose (hide/continue): ').lower()
-                    if answer == 'hide':
-                        print('You hide inside the house for several hours and eventually a group of cannibals finds you and you die.\nTry again next time! Thanks for playing!')
-                        break
-                    elif answer == 'continue':
-                        answer = input('As you walk through the woods you come across some arrows on the ground that will serve as ammo for your crossbow. Suddenly you hear a motorcycle approaching and you see two ax-wielding men. Now you have a decision to make, do you try to kill these men with your crossbow or do you run back to the house and hide? Please Choose (house/shoot): ').lower()
-                        if answer == 'shoot':
-                            print('Your crossbow jammed and they killed you.\nTry again next time! Thanks for playing!')
+        if answer == 'n':
+            break
+        if answer == 'y':
+            answer = input("You've just woken up in a strange room. You hear a faint sound coming from upstairs and it sounds like something in distress. You also see what you think are flashlights roaming around outside. Do you follow the sound or the lights? Please Choose (sound/lights): ").lower()
+            if answer == 'sound':
+                answer = input('You head upstairs toward the sound and find a locked armoire. It sounds like there is a rabid dog inside, do you open it or go back downstairs? Please Choose (open/leave): ').lower()
+                points += 1
+                if answer == 'leave':
+                    print('You turn around to go back but fall down the stairs and break your neck.\nTry again next time! Thanks for playing!')
+                    break
+                elif answer == 'open':
+                    answer = input('A dog greets you with kisses, happy to be saved. Inside the armoire you find a crossbow but no arrows, do you take it or leave it behind? Please Choose (take it/leave it): ').lower()
+                    points += 1
+                    if answer == 'take it':
+                        answer = input('You head downstairs and lucky for you the dog notices a big hole in the ground, so you step around it. You start calling for help moving toward the lights in the woods and you notice the lights begin to charge at you. It is a very large man looking to harm you but once he is close enough the dog jumps to your rescue and attcks. He successfully protects you but dies in the tussle. Do you go back into the house and hide or do you take the flashlight from the corpse and continue walking through the woods? Please Choose (hide/continue): ').lower()
+                        points += 1
+                        if answer == 'hide':
+                            print('You hide inside the house for several hours and eventually a group of cannibals finds you and you die.\nTry again next time! Thanks for playing!')
                             break
-                        elif answer == 'house':
-                            print('You realize the house was the home of your attackers and as you anxiously look around you find a shotgun. When they run inside looking for you, you shoot them with the shotgun.\nNow at ease, you look for something to call for help but instead find dismembered body parts and you relaize these men were cannibals looking for their next meal. You decide to take their motorcycle and ride until you reach civilization. Congrats you have made it through this adventure!')
-                            break
+                        elif answer == 'continue':
+                            answer = input('As you walk through the woods you come across some arrows on the ground that will serve as ammo for your crossbow. Suddenly you hear a motorcycle approaching and you see two ax-wielding men. Now you have a decision to make, do you try to kill these men with your crossbow or do you run back to the house and hide? Please Choose (house/shoot): ').lower()
+                            points += 1
+                            if answer == 'shoot':
+                                print('Your crossbow jammed and they killed you.\nTry again next time! Thanks for playing!')
+                                break
+                            elif answer == 'house':
+                                print('You realize the house was the home of your attackers and as you anxiously look around you find a shotgun. When they run inside looking for you, you shoot them with the shotgun.\nNow at ease, you look for something to call for help but instead find dismembered body parts and you relaize these men were cannibals looking for their next meal. You decide to take their motorcycle and ride until you reach civilization. Congrats you have made it through this adventure!')
+                                points += 1 
+                                print(f'Congrats, you gained {points} points this round!')
+                                break
+                            else:
+                                print('Not a valid option, you died.\nThanks for playing!')
+                                break
                         else:
                             print('Not a valid option, you died.\nThanks for playing!')
                             break
-                    else:
+                    elif answer == 'leave it':
+                        answer = input('You head downstairs and lucky for you the dog notices a big hole in the ground, so you step around it. You start calling for help moving towards the lights in the woods and you notice the lights begin to charge at you. It is a very large man looking to harm you but once he is close enough the dog jumps to your rescue and attcks. He successfully protects you but dies in the tussle. Do you go back into the house and hide or do you take the flashlight from the corpse and continue walking through the woods? Please Choose (hide/continue): ').lower()
+                        points += 1
+                        if answer == 'hide':
+                            print('You hide inside the house for several hours and eventually a group of cannibals finds you and you die.\nTry again next time! Thanks for playing!')
+                            break
+                        elif answer == 'continue':
+                            answer = input('As you walk through the woods you come across some arrows on the ground and you pick them up hoping to use them as defense. Suddenly you hear a motorcycle approaching and you see two ax-wielding men. Now you have a decision to make, do you try to attack these men with your arrows or do you run back to the house and hide? Please Choose (attack/run): ').lower()
+                            points += 1
+                            if answer == 'run':
+                                print('You realize the house was the home of your attackers and as you anxiously look around you find a shotgun. When they run inside looking for you, you shoot them with the shotgun.\nNow at ease, you look for something to call for help but instead find dismembered body parts and you relaize these men were cannibals looking for their next meal. You decide to take their motorcycle and ride until you reach civilization. Congrats you have made it through this adventure!')
+                                points += 1
+                                print(f'Congrats, you gained {points} points this round!')
+                                break
+                            elif answer == 'attack':
+                                print('Your arrows were no match for their axes and they killed you, you lost!\nTry again next time! Thanks for playing!')
+                                break
+                            else: 
+                                print('Not a valid option, you died.\nThanks for playing!')
+                                break
+                    else: 
                         print('Not a valid option, you died.\nThanks for playing!')
                         break
-                elif answer == 'leave it':
-                    answer = input('You head downstairs and lucky for you the dog notices a big hole in the ground, so you step around it. You start calling for help moving towards the lights in the woods and you notice the lights begin to charge at you. It is a very large man looking to harm you but once he is close enough the dog jumps to your rescue and attcks. He successfully protects you but dies in the tussle. Do you go back into the house and hide or do you take the flashlight from the corpse and continue walking through the woods? Please Choose (hide/continue): ')
-                    if answer == 'hide':
-                        print('You hide inside the house for several hours and eventually a group of cannibals finds you and you die.\nTry again next time! Thanks for playing!')
-                        break
-                    elif answer == 'continue':
-                        answer = input('As you walk through the woods you come across some arrows on the ground and you pick them up hoping to use them as defense. Suddenly you hear a motorcycle approaching and you see two ax-wielding men. Now you have a decision to make, do you try to attack these men with your arrows or do you run back to the house and hide? Please Choose (attack/run): ')
-                        if answer == 'run':
-                            print('You realize the house was the home of your attackers and as you anxiously look around you find a shotgun. When they run inside looking for you, you shoot them with the shotgun.\nNow at ease, you look for something to call for help but instead find dismembered body parts and you relaize these men were cannibals looking for their next meal. You decide to take their motorcycle and ride until you reach civilization. Congrats you have made it through this adventure!')
-                            break
-                        elif answer == 'attack':
-                            print('Your arrows were no match for their axes and they killed you, you lost!\nTry again next time! Thanks for playing!')
-                            break
-                        else: 
-                            print('Not a valid option, you died.\nThanks for playing!')
-                            break
                 else: 
                     print('Not a valid option, you died.\nThanks for playing!')
                     break
+            elif answer == 'lights':
+                answer = input('You run outside toward the lights and fall into a hole before you could get close to them. Inside the hole you find a small tunnel, do you go through it or stay in place and call for help? Please Choose (tunnel/help): ').lower()
+                points += 1
+                if answer == 'tunnel':
+                    answer = input('You crawl inside looking for something and find a flashlight jammed into the wall of the tunnel, do you pull it out or continue without it? Please Choose (pull/continue): ').lower()
+                    points += 1
+                    if answer == 'pull':
+                        print('You caused the tunnel to collapse in on itself and you died from suffocation!\nTry again next time! Thanks for playing!')
+                        break
+                    elif answer == 'continue':
+                        print('As you went on your knee hit the flashlight causing the tunnel to become unstable and collapse in on itself, you are now dead.\nTry again next time! Thanks for playing!')
+                        break
+                    else:
+                        print('Not a valid option, you died.\nThanks for playing!')
+                elif answer == 'help':
+                    print('After yelling for help the lights make their way toward you and find you in the hole. It is a cannibalistic man looking for food and you have just made his day. Sorry but you died, maybe you should have gone upstairs.\nTry again next time! Thanks for playing!')
+                    break
+                else:
+                    print('Not a valid option, you died.\nThanks for playing!')
             else: 
                 print('Not a valid option, you died.\nThanks for playing!')
                 break
-        elif answer == 'lights':
-            answer = input('You run outside toward the lights and fall into a hole before you could get close to them. Inside the hole you find a small tunnel, do you go through it or stay in place and call for help? Please Choose (tunnel/help): ').lower()
-            if answer == 'tunnel':
-                answer = input('You crawl inside looking for something and find a flashlight jammed into the wall of the tunnel, do you pull it out or continue without it? Please Choose (pull/continue): ').lower()
-                if answer == 'pull':
-                    print('You caused the tunnel to collapse in on itself and you died from suffocation!\nTry again next time! Thanks for playing!')
-                elif answer == 'continue':
-                    print('As you went on your knee hit the flashlight causing the tunnel to become unstable and collapse in on itself, you are now dead.\nTry again next time! Thanks for playing!')
-                else:
-                    print('Not a valid option, you died.\nThanks for playing!')
-            elif answer == 'help':
-                print('After yelling for help the lights make their way toward you and find you in the hole. It is a cannibalistic man looking for food and you have just made his day. Sorry but you died, maybe you should have gone upstairs.\nTry again next time! Thanks for playing!')
-            else:
-                print('Not a valid option, you died.\nThanks for playing!')
-        else: 
-            print('Not a valid option, you died.\nThanks for playing!')
+        else:
+            print('Not a valid option, please try again.')
             break
-    else:
-        print('Not a valid option, please try again.')
-        break
+    
+    Result.create(player.id, points)
+
+if __name__ == "__main__":
+    Player.create_table()
+    Player.get_all()
+    Result.create_table()
+    Result.get_all()
+    main()
--- lib/models/classes.py
+++ lib/models/classes.py
@@ -1,17 +1,89 @@
 import sqlite3
 import ipdb 
 
-CONN = sqlite3.connect('company.db')
+CONN = sqlite3.connect('game_results.db')
 CURSOR = CONN.cursor()
 
 class Player:
+
+    all = []
+
     def __init__(self, username):
+        self.id = None
         self.username = username
         self.results_list = []
 
-    def results(self):
-        return self.results_list
+    def __repr__(self):
+        repr_string = ""
+        repr_string += f'Player: {self.username}'
 
+        if not len(self.results_list) == 0:
+            print("Here is this player's results:")
+            for result in self.results_list:
+                print(result)
+
+        return repr_string
+
+    @classmethod
+    def create_table(cls):
+        sql = '''
+            CREATE TABLE IF NOT EXISTS players (
+                id INTEGER PRIMARY KEY,
+                username TEXT
+            )
+        '''
+        CURSOR.execute(sql)
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS players
+        """
+        CURSOR.execute(sql)
+
+    @classmethod
+    def player_in_db(cls, row):
+        player = cls(row[1])
+        player.id = row[0]
+        return player
+    
+    @classmethod
+    def get_all(cls):
+        sql = '''
+            SELECT *
+            FROM players
+        '''
+        
+        all = CURSOR.execute(sql).fetchall()
+        cls.all = [cls.player_in_db(row) for row in all]
+        return cls.all
+
+    @classmethod
+    def find_by_id(cls, id):
+        players = [player for player in Player.all if player.id == id]
+
+        if players:
+            return players[0]
+        else:
+            return None
+
+    @classmethod
+    def create(cls, username):
+        player = Player(username)
+        player.save()
+        cls.all.append(player)
+        return player
+
+    def save(self):
+        sql = """
+            INSERT INTO players (username)
+            VALUES (?)
+        """
+
+        CURSOR.execute(sql, (self.username,))
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM players").fetchone()[0]
+        CONN.commit()
+    
     @property
     def username(self):
         return self._username
@@ -22,11 +94,82 @@ class Player:
         else: 
             print('Username can only be letters and must be at least 3 charcaters.')
     
-class Results:
-    def __init__(self, score, player):
-        self.score = score
-        self.player = player
+class Result:
+
+    all = []
+
+    def __init__(self, player_id, points):
+        self.id = None
+        self.points = points
+        self.player_id = player_id
+        self.player = Player.find_by_id(player_id)
         self.player.results_list.append(self)
+
+    def __repr__(self):
+        return f'Result # {self.id}: {self.player.username} has {self.points}'
+
+    @classmethod
+    def create_table(cls):
+        sql = '''
+            CREATE TABLE IF NOT EXISTS results (
+                id INTEGER PRIMARY KEY,
+                player_id INTEGER,
+                points INTEGER
+            )
+        '''
+        CURSOR.execute(sql)
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS results
+        """
+        CURSOR.execute(sql)
+
+        cls.all = []
+
+    @classmethod
+    def result_in_db(cls, row):
+        result = cls(row[1], row[2])
+        result.id = row[0]
+        return result
+    
+    @classmethod
+    def get_all(cls):
+        sql = '''
+            SELECT *
+            FROM results
+        '''
+        
+        all = CURSOR.execute(sql).fetchall()
+        cls.all = [cls.result_in_db(row) for row in all]
+        return cls.all
+
+    @classmethod
+    def create(cls, player_id, points):
+        result = Result(player_id, points)
+        result.save()
+        cls.all.append(result)
+        return result
+
+    def save(self):
+        sql = """
+            INSERT INTO results (player_id, points)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.player_id, self.points))
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM results").fetchone()[0]
+        CONN.commit()
+
+    # def update(self):
+    #     sql = '''
+    #         UPDATE results
+    #         SET player_id = ?, points = ?
+    #         WHERE id = ?
+    #     '''
+    #     CURSOR.execute(sql, (self.player_id, self.points, self.id))
+    #     CONN.commit()
 
     @property
     def player(self):
@@ -35,29 +178,13 @@ class Results:
     def player(self, player):
         if type(player) == Player:
             self._player = player
-
-class Game:
-    def __init__(self, title):
-        self.title = title
-        self.results_list = []
-        self.players_list = []
-    
-    def results(self):
-        return self.results_list
-    
-    def players(self):
-        return self.players_list
+        else:
+            raise Exception(f'Error: Player {self.player_id} does not exist!')
 
     @property
-    def title(self):
-        return self._title
-    @title.setter
-    def title(self, title):
-        pass 
-
-
-
-
-
-# p = Player('A')
-# ipdb.set_trace()
+    def points(self):
+        return self._points
+    @points.setter
+    def points(self, points):
+        if type(points) == int:
+            self._points = points
